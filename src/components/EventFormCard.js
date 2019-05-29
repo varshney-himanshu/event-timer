4f361db37934b43e990c718bfbed7ee425dd89be
partofice <officialpartofice@gmail.com>
2019-05-29 15:58:24 +0530

    initial commit

---
+++ src/components/EventFormCard.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+class EventFormCard extends React.Component{
+     
+    constructor(){
+        super();
+        this.state={
+            eName : "",     //eName = Event Name
+            day : "",       //day = Deadline of the Event
+            time : ""       //time = time of the event
+        }  
+
+    }
+
+    componentDidMount(){
+      var dateInput = document.querySelector('input[type=date]');  //-----------start------------------
+      var todaysDate = new Date();
+      var year = todaysDate.getFullYear();                            //this block of code restricts the entry of earlier
+      var month = ("0" + (todaysDate.getMonth() + 1)).slice(-2);      //date then current date
+      var day = ("0" + todaysDate.getDate()).slice(-2); 
+      var minDate = (year +"-"+ month +"-"+ day);     
+      dateInput.setAttribute("min",minDate);                  //---------------end----------------
+      
+    }
+
+    oninputchange = (e)=>{   //oninputchange : updates the state of the component as the input changes
+        this.setState({[e.target.name] : e.target.value}) 
+    }
+
+    onsubmit = (e)=>{                        //onsubmit : submits the form data to form component through props
+        e.preventDefault();
+        const {eName,day,time} = this.state;  
+        
+        var d = day.split("-");
+        var t = time.split(":");  
+         
+         var date = d[1].toString() + " " + d[2].toString() + " " + d[0].toString() + " " + t[0].toString() + ":" + t[1].toString() + ":00";
+         
+         const dline = new Date(date);
+
+        const data = {
+                 eName,
+                 deadline : dline,
+             }
+        this.props.sendEventData(data);
+    }
+
+    render(){
+      return(
+      <form className="eventformcard mx-auto mt-3" onSubmit={this.onsubmit} >
+          <h2 style={{textAlign:"center"}}> Create an event </h2>
+        <div className="form-group"> 
+         <label htmlFor="eName">Event Name</label>
+         <input type="text" className="form-control form-control-sm" name="eName" onChange={this.oninputchange} required/> 
+        </div>
+        <div className="form-group">
+         <label htmlFor="day">Day</label>  
+         <input type="date" className="form-control form-control-sm" name="day" onChange={this.oninputchange} required/>  
+        </div>  
+        <div className="form-group">
+         <label htmlFor="time">Time</label>  
+         <input type="time" className="form-control form-control-sm" name="time" onChange={this.oninputchange} required/>           </div>           
+      
+         <input type="submit" className="btn btn-block btn-success " value="Start Event Timer"/>
+      </form>
+      );
+    }
+}
+
+
+export default EventFormCard;
---
+++ src/components/EventTimer.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+class EventTimer extends React.Component{
+   
+   constructor(){
+    super();
+     
+    this.state = {
+         timerDisplay : "",
+     }
+
+   }
+
+   componentDidMount(){
+  
+     if(this.props.deadline){
+       this.interval = setInterval(()=>{
+         var now = new Date();
+         var rt = this.props.deadline - now;
+         var days = Math.floor((rt/(1000*60*60*24)));
+         var hours = Math.floor(((rt%(1000*60*60*24))/(1000*60*60)));
+         var minutes = Math.floor(((rt%(1000*60*60))/(1000*60)));
+         var seconds = Math.floor((rt%(1000*60))/1000);  
+          
+          var str = days.toString() + ":" + hours.toString() + ":" + minutes.toString() + ":" + seconds.toString();
+         
+         this.setState({timerDisplay : str});
+        if(rt<=0) {
+          this.setState({timerDisplay : "0:0:0:0"});
+          clearInterval(this.interval);
+        } 
+     },1000);
+    }
+   } 
+
+   stopTimer = ()=>{
+       this.props.stopTimer(this.props.eventId);
+        //console.log("stop timer in EventTimer!!")
+   }
+    
+    render(){
+        return(
+            <div className="eventtimer mx-auto">
+               <p>{this.props.eName}</p>
+              <h1>{this.state.timerDisplay}</h1> 
+              <button onClick={this.stopTimer} className="btn btn-outline-danger">Stop Timer</button>
+            </div>
+        );
+    }
+
+}
+
+export default EventTimer;
